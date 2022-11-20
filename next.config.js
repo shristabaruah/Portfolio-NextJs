44cba26183f4895e0494a4760837ed2b22119cbc
Shrista Baruah <44132087+shristabaruah@users.noreply.github.com>
2022-11-20 13:57:11 +0530

    feat-portfolio

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // images: {
+  //   loader: "cloudinary",
+  //   path: "https://res.cloudinary.com/dopc1gfoj/image/upload",
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
